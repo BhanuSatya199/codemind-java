4f6aabe8ecac9d2149854981ed87d6cd57af6215
BhanuSatya199 <153795402+BhanuSatya199@users.noreply.github.com>
2024-08-02 09:34:43 +0530

    import java.util.Scanner; public class Demo {     public static void main(String[] args)     {         int a,b,c;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         c=a*5+b*7;         System.out.println(c);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        c=a*5+b*7;
+        System.out.println(c);
+    }
+}
